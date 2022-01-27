a141ea15c50f624c1e3f3816237766b62901f4b5
Kiran Mohan <kiran.mohan@123.com>
2022-01-27 18:42:17 +0530

    Initial commit

---
+++ src/main/java/org/ktest/Example.java
@@ -0,0 +1,52 @@
+package org.ktest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.util.Arrays;
+
+@Configuration
+@PropertySource(value = "classpath:example.yml", factory = YamlPropertySourceFactory.class)
+public class Example {
+
+    @Value("${example.enabled}")
+    private boolean enabled;
+    @Value("${example.name}")
+    private String name;
+    @Value("${example.myArray}")
+    private String[] myArray;
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getMyArray() {
+        return myArray;
+    }
+
+    public void setMyArray(String[] myArray) {
+        this.myArray = myArray;
+    }
+
+    @Override
+    public String toString() {
+        return "Example{" +
+                "enabled=" + enabled +
+                ", name='" + name + '\'' +
+                ", myArray=" + Arrays.toString(myArray) +
+                '}';
+    }
+}
---
+++ src/test/java/org/ktest/ExampleTest.java
@@ -0,0 +1,32 @@
+package org.ktest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("classpath:spring/example.xml")
+@Slf4j
+class ExampleTest {
+
+    @Autowired
+    private Example example;
+
+    @Test
+    void test() {
+        log.debug("enabled: {}",example.isEnabled());
+        log.debug("name: {}",example.getName());
+        log.debug("myArray: {}",example.getMyArray());
+
+        assertTrue(example.isEnabled());
+        assertEquals("org.ktest", example.getName());
+        assertArrayEquals(new String[]{"abc", "def"}, example.getMyArray());
+
+    }
+
+}
